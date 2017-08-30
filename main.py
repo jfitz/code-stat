5b565b5f9e1bbce65c2405cbeb1c3175c02e7a9d
John Fitzpatrick <jfitz@computer.org>
2017-08-30 09:42:00 -0700

    Change output to HTML from jinja template.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World from Azure!'
+  html = render_template('index.jinja.txt', title='CodeStat')
+  return html
+
+@app.route('/linecount/', methods=['POST'])
+def linecount():
+  text = request.form
+  return str(text)
 
 if __name__ == '__main__':
   app.run()
