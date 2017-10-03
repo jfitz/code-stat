c5c58f5dc0bcf46155712fb5637a58108ceda30b
John Fitzpatrick <jfitz@computer.org>
2017-10-02 20:01:35 -0700

    Change Pascal parsing to tokenizer method.

--- PascalExaminer.py
+++ PascalExaminer.py
@@ -1,48 +1,78 @@
+import string
 from Examiner import Examiner
+from PascalTokenBuilders import *
+from Tokenizer import Tokenizer
 
 class PascalExaminer(Examiner):
   def __init__(self, code):
-    confidence_1 = 0
-    confidence_2 = 0
+    lines = code.split('\r\n')
+
+    num_tokens = 0
+    num_known_tokens = 0
+
+    wtb = WhitespaceTokenBuilder()
+    ntb = NumberTokenBuilder()
+    itb = IdentifierTokenBuilder()
+    stb = StringTokenBuilder()
+    bctb = BraceCommentTokenBuilder()
+    ctb = CommentTokenBuilder()
+    operators = [
+      '+', '-', '*', '/', '^',
+      '=', '>', '>=', '<', '<=', '<>',
+      '(', ')', ',', ':', ';',
+      '[', ']', '..'
+      ]
+    otb = ListTokenBuilder(operators)
+    tokenbuilders = [wtb, ntb, itb, stb, otb, bctb, ctb]
+
+    
+    invalid_token_builder = InvalidTokenBuilder()
+    tokenizer = Tokenizer(tokenbuilders, invalid_token_builder)
+
     first_token = ''
     last_token = ''
     num_begin = 0
     num_end = 0
 
-    lines = code.split('\r\n')
+    confidence_1 = 0
+    confidence_2 = 0
+    confidence_3 = 0
+    
     for line in lines:
-      line = self.remove_pascal_comments(line)
-      # remove leading and trailing blanks
       line = line.strip()
       #  consider only lines with text
       if len(line) > 0:
-        line = self.remove_string_literals(line)
         #  simple lexer
-        tokens = self.split_to_tokens(line)
-        #  merge adjacent tokens when compatible
-        #  drop all numbers
-        tokens = self.drop_numbers(tokens)
-        #  count 'begin' and 'end' tokens
+        tokens = tokenizer.tokenize(line)
+        #  unknown operators reduce confidence
+        #  unknown identifiers (text of two or more, not FNx) reduce confidence
         for token in tokens:
+          num_tokens += 1
+          if not token.invalid:
+            num_known_tokens += 1
           if first_token == '':
             first_token = token
           last_token = token
-          if token == 'begin':
+          if str(token) == 'begin':
             num_begin += 1
-          if token == 'end':
+          if str(token) == 'end':
             num_end += 1
 
-    if first_token == 'program':
+    if str(first_token) == 'program':
       confidence_1 += 0.5
 
-    if last_token == '.':
+    if str(last_token) == '.':
       confidence_1 += 0.5
 
     if num_begin == num_end and num_begin > 0:
       confidence_2 = 1.0
 
+    confidence_3 = 0
+    if num_tokens > 0:
+      confidence_3 = num_known_tokens / num_tokens
+
     # compute confidence
-    self.confidence = confidence_1 * confidence_2
+    self.confidence = confidence_1 * confidence_2 * confidence_3
 
   def confidence(self):
     return self.confidence
---
+++ PascalTokenBuilders.py
@@ -0,0 +1,265 @@
+class Token:
+  def __init__(self, text, invalid):
+    self.text = text
+    self.invalid = invalid
+
+  def __str__(self):
+    return self.text
+
+  def count(self):
+    return len(self.text)
+
+# accept any character (but only one)
+class InvalidTokenBuilder:
+  def __init__(self):
+    self.token = None
+
+  def attempt(self, text):
+    self.token = None
+    if len(text) > 0:
+      self.token = text[0]
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, True)
+  
+# token reader for whitespace
+class WhitespaceTokenBuilder:
+  def __init__(self):
+    self.token = None
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    return c.isspace()
+
+# token reader for number
+class NumberTokenBuilder:
+  def __init__(self):
+    self.token = None
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    return c.isdigit()
+
+# token reader for identifier
+class IdentifierTokenBuilder:
+  def __init__(self):
+    self.token = None
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    result = None
+    if result is None and c.isalpha():
+      result = True
+    if result is not None and c.isdigit():
+      result = True
+    if result is not None and (c == '$' or c == '%'):
+      result = True
+    return result
+
+# token reader for text literal (string)
+class StringTokenBuilder:
+  def __init__(self):
+    self.token = ''
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    result = False
+    if c == "'" and candidate == '':
+      result = True
+    if c == "'" and len(candidate) > 0:
+      result = True
+    if c != "'" and len(candidate) == 1:
+      result = True
+    if c != "'" and len(candidate) > 1 and candidate[-1] != "'":
+      result = True
+    return result
+
+# accept characters to match item in list
+class ListTokenBuilder:
+  def __init__(self, legals):
+    self.legals = legals
+    self.token = ''
+
+  def attempt(self, text):
+    self.token = None
+    best_candidate = ''
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+        i += 1
+        if candidate in self.legals:
+          best_candidate = candidate
+
+    if len(best_candidate) > 0:
+      self.token = best_candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    token = candidate + c
+    count = len(token)
+    result = False
+
+    for legal in self.legals:
+      if legal[:count] == token:
+        result = True
+
+    return result
+
+# token reader for brace comment
+class BraceCommentTokenBuilder:
+  def __init__(self):
+    self.token = ''
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    result = False
+    if c == '{' and candidate == '':
+      result = True
+    if c == '}' and len(candidate) > 0:
+      result = True
+    if len(candidate) > 1 and candidate[-1] != '}':
+      result = True
+    return result
+
+# token reader for comment
+class CommentTokenBuilder:
+  def __init__(self):
+    self.token = ''
+
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
+  def get_token(self):
+    if self.token is None:
+      return None
+    return Token(self.token, False)
+
+  def accept(self, candidate, c):
+    result = False
+    if c == '(' and candidate == '':
+      result = True
+    if c == '*' and len(candidate) == 1:
+      result = True
+    if c == ')' and len(candidate) > 2 and candidate[-1] == '*':
+      result = True
+    if candidate.startswith('(*') and candidate[-1] != ')':
+      result = True
+    return result
