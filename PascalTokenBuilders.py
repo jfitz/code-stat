f319ab86d7e156fac9b9120e7ba6de933c9a3366
John Fitzpatrick <jfitz@computer.org>
2017-11-05 17:11:55 -0800

    Consolidate duplicate functions into a single function in parent TokebBuilder class.

--- PascalTokenBuilders.py
+++ PascalTokenBuilders.py
@@ -1,24 +1,10 @@
 from Token import Token
+from TokenBuilders import TokenBuilder
 
 # token reader for identifier
-class IdentifierTokenBuilder:
+class IdentifierTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
@@ -36,24 +22,9 @@ class IdentifierTokenBuilder:
     return result
 
 # token reader for text literal (string)
-class StringTokenBuilder:
+class StringTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = ''
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
@@ -73,24 +44,9 @@ class StringTokenBuilder:
     return result
 
 # token reader for brace comment
-class BraceCommentTokenBuilder:
+class BraceCommentTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = ''
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
@@ -108,24 +64,9 @@ class BraceCommentTokenBuilder:
     return result
 
 # token reader for comment
-class CommentTokenBuilder:
+class CommentTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = ''
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
--- TokenBuilders.py
+++ TokenBuilders.py
@@ -1,7 +1,24 @@
 from Token import Token
 
+# generic TokenBuilder (to hold common functions)
+class TokenBuilder:
+  def attempt(self, text):
+    self.token = None
+    candidate = ''
+    i = 0
+    accepted = True
+    while i < len(text) and accepted:
+      c = text[i]
+      accepted = self.accept(candidate, c)
+      if accepted:
+        candidate += c
+      i += 1
+
+    if len(candidate) > 0:
+      self.token = candidate
+
 # accept any character (but only one)
-class InvalidTokenBuilder:
+class InvalidTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
 
@@ -16,24 +33,9 @@ class InvalidTokenBuilder:
     return Token(self.token, 'invalid')
   
 # token reader for whitespace
-class WhitespaceTokenBuilder:
+class WhitespaceTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
@@ -44,24 +46,9 @@ class WhitespaceTokenBuilder:
     return c.isspace()
 
 # token reader for number
-class NumberTokenBuilder:
+class NumberTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
@@ -72,7 +59,7 @@ class NumberTokenBuilder:
     return c.isdigit()
 
 # accept characters to match item in list
-class ListTokenBuilder:
+class ListTokenBuilder(TokenBuilder):
   def __init__(self, legals, group):
     self.legals = legals
     self.group = group
@@ -113,24 +100,9 @@ class ListTokenBuilder:
     return result
 
 # token reader for newline
-class NewlineTokenBuilder:
+class NewlineTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
-
-  def attempt(self, text):
-    self.token = None
-    candidate = ''
-    i = 0
-    accepted = True
-    while i < len(text) and accepted:
-      c = text[i]
-      accepted = self.accept(candidate, c)
-      if accepted:
-        candidate += c
-      i += 1
-
-    if len(candidate) > 0:
-      self.token = candidate
 
   def get_token(self):
     if self.token is None:
