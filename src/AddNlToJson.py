aadd93a4c11e08eaec00a5d5b23e9e8e366b0d4d
John Fitzpatrick <jfitz@computer.org>
2018-11-16 21:16:46 -0800

    Revise pretty-printing of JSON to have better line breaks.

--- src/AddNlToJson.py
+++ src/AddNlToJson.py
@@ -3,21 +3,38 @@ import fileinput
 for line in fileinput.input():
     line = line.rstrip('\r\n')
 
+    start_of_file = True
+    start_of_line = True
     in_quotes = False
-    prev_was_escape = False 
+    prev_was_escape = False
+    prev_was_group = False
+    prev_was_comma = False
 
     for char in line:
         if in_quotes:
             if char == '"' and not prev_was_escape:
                 in_quotes = False
+
             print(char, end = '')
             prev_was_escape = char == '\\'
             
         else:
             if char == '"':
                 in_quotes = True
-            if char in ['{', '}'] and not in_quotes:
+
+            if char in ['}', ']']:
                 print()
+
+            if prev_was_group and char not in ['}', ']', ',']:
+                print()
+
+            if prev_was_comma:
+                print()
+
             print(char, end = '')
-            if char in ['{', '}', ','] and not in_quotes:
-                print()
+
+            prev_was_group = char in ['{', '}', '[', ']']
+            prev_was_comma = char == ','
+
+        start_of_file = False
+
