e0c00e43652a18d82c83c62b1936210aad90daee
John Fitzpatrick <jfitz@computer.org>
2018-12-05 20:22:04 -0800

    Fix file names for 'detab' tests.

--- test/bin/run_detab_tests.ps1
+++ test/bin/run_detab_tests.ps1
@@ -2,9 +2,9 @@ Set-StrictMode -Version Latest
 
 [int]$failures = 0
 
-test\bin\run_test.ps1 -name "Cobol Program 1" -action detab -inputfile ".\test\data\COBOL\Cobol Program 1.COB" -expected ".\test\ref\detab\COBOL\Cobol Program 1.txt"
-test\bin\run_test.ps1 -name "Cobol Program 2" -action detab -inputfile ".\test\data\COBOL\Cobol Program 2.COB" -expected ".\test\ref\detab\COBOL\Cobol Program 2.txt"
-test\bin\run_test.ps1 -name "Cobol Program 3" -action detab -inputfile ".\test\data\COBOL\Cobol Program 3.COB" -expected ".\test\ref\detab\COBOL\Cobol Program 3.txt"
-test\bin\run_test.ps1 -name "Cobol Program 4" -action detab -inputfile ".\test\data\COBOL\Cobol Program 4.COB" -expected ".\test\ref\detab\COBOL\Cobol Program 4.txt"
+test\bin\run_test.ps1 -name freeformat1 -action detab -inputfile ".\test\data\COBOL\Free Format 1.COB" -expected ".\test\ref\detab\COBOL\Free Format 1.txt"
+test\bin\run_test.ps1 -name freeformat2 -action detab -inputfile ".\test\data\COBOL\Free Format 2.COB" -expected ".\test\ref\detab\COBOL\Free Format 2.txt"
+test\bin\run_test.ps1 -name freeformat3 -action detab -inputfile ".\test\data\COBOL\Free Format 3.COB" -expected ".\test\ref\detab\COBOL\Free Format 3.txt"
+test\bin\run_test.ps1 -name freeformat4 -action detab -inputfile ".\test\data\COBOL\Free Format 4.COB" -expected ".\test\ref\detab\COBOL\Free Format 4.txt"
 
 Write-Output "Number of failures: $failures"
