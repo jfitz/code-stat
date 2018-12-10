ab4753110e8bd02e00aff51843ad2a54a58d4c08
John Fitzpatrick <jfitz@computer.org>
2018-12-09 18:42:00 -0800

    Rename 'tokenizers' to 'tokenbuilders'.

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,7 +1,7 @@
 class Tokenizer():
-  def __init__(self, tokenizers, invalid_tokenizer):
-    self.tokenizers = tokenizers
-    self.invalid_tokenizer = invalid_tokenizer
+  def __init__(self, tokenbuilders, invalid_tokenbuilder):
+    self.tokenbuilders = tokenbuilders
+    self.invalid_tokenbuilder = invalid_tokenbuilder
 
   def tokenize(self, text):
     tokens = []
@@ -9,46 +9,46 @@ class Tokenizer():
     while len(text) > 0:
       new_tokens = None
 
-      tokenizer = self.try_tokenizers(text)
+      tokenbuilder = self.try_tokenbuilders(text)
 
-      if tokenizer is not None:
-        new_tokens = tokenizer.get_tokens()
+      if tokenbuilder is not None:
+        new_tokens = tokenbuilder.get_tokens()
       else:
-        if self.invalid_tokenizer != None:
-          tokenizer = self.invalid_tokenizer
+        if self.invalid_tokenbuilder != None:
+          tokenbuilder = self.invalid_tokenbuilder
           new_tokens = self.try_invalid(text)
 
       if new_tokens == None:
         raise Exception("Cannot tokenize '" + text + "'")
 
       tokens += new_tokens
-      count = tokenizer.get_count()
+      count = tokenbuilder.get_count()
       text = text[count:]
     
     return tokens
     
-  def try_tokenizers(self, text):
+  def try_tokenbuilders(self, text):
     # try all tokenbuilders
-    for tokenizer in self.tokenizers:
-      tokenizer.attempt(text)
+    for tokenbuilder in self.tokenbuilders:
+      tokenbuilder.attempt(text)
 
-    # see which tokenbuilder found the longest
+    # see which tokenbuilder found the highest score
     winner = None
     winner_score = 0
-    for tokenizer in self.tokenizers:
-      t = tokenizer.get_tokens()
-      score = tokenizer.get_score()
+    for tokenbuilder in self.tokenbuilders:
+      t = tokenbuilder.get_tokens()
+      score = tokenbuilder.get_score()
 
       if t is not None:
         if score > winner_score:
-          winner = tokenizer
+          winner = tokenbuilder
           winner_score = winner.get_score()
 
     return winner
 
   def try_invalid(self, text):
-    self.invalid_tokenizer.attempt(text)
-    tokens = self.invalid_tokenizer.get_tokens()
+    self.invalid_tokenbuilder.attempt(text)
+    tokens = self.invalid_tokenbuilder.get_tokens()
     if tokens is None:
       return None
 
