26d89d5e9ef7c6dbe475824220d54beab05f2ba9
John Fitzpatrick <jfitz@computer.org>
2019-01-04 20:16:32 -0800

    Change tokenization of FORTRAN format specifiers to allow for repeated values.

--- FortranXTokenBuilders.py
+++ FortranXTokenBuilders.py
@@ -1,4 +1,5 @@
 import string
+import re
 from Token import Token
 from TokenBuilders import TokenBuilder
 
@@ -60,6 +61,21 @@ class LineNumberTokenBuilder(TokenBuilder):
 class FormatSpecifierTokenBuilder(TokenBuilder):
   def __init__(self):
     self.token = None
+    # 'p' patterns are approved, 't' patterns are for accept()
+    self.t1 = re.compile('\A\d+\Z')
+    self.p2 = re.compile('\A\d*[IFEDGAL]\Z')
+    self.p3 = re.compile('\A\d*[IFEDG]\d+\Z')
+    self.t4 = re.compile('\A\d*[IFEDG]\d+\.\Z')
+    self.p5 = re.compile('\A\d*[FEDG]\d+\.\d+\Z')
+    self.t6 = re.compile('\A\d*[FEDG]\d+\.\d+E\Z')
+    self.p7 = re.compile('\A\d*[FEDG]\d+\.\d+E\d+\Z')
+
+    self.approveds = [
+      self.p2,
+      self.p3,
+      self.p5,
+      self.p7
+    ]
 
 
   def get_tokens(self):
@@ -73,10 +89,28 @@ class FormatSpecifierTokenBuilder(TokenBuilder):
     result = False
 
     if len(candidate) == 0:
-      result = c in 'IFEDGALX'
+      result = c.isdigit() or c in 'IFEDGALXP'
 
-    if len(candidate) > 0:
-      result = c.isdigit() or (c == '.' and '.' not in candidate)
+    if self.t1.match(candidate):
+      result = c.isdigit() or c in 'IFEDGALXP'
+
+    if self.p2.match(candidate):
+      result = c.isdigit()
+
+    if self.p3.match(candidate):
+      result = c.isdigit() or c == '.'
+
+    if self.t4.match(candidate):
+      result = c.isdigit()
+
+    if self.p5.match(candidate):
+      result = c == 'E'
+
+    if self.t6.match(candidate):
+      result = c.isdigit()
+
+    if self.p7.match(candidate):
+        result = c.isdigit()
 
     return result
 
@@ -85,12 +119,19 @@ class FormatSpecifierTokenBuilder(TokenBuilder):
     if self.token is None:
       return 0
 
-    if self.token[-1] == '.':
-      return 0
-
-    # if line is a FORMAT line, we're OK
+    in_format = False
     for token in line_printable_tokens:
       if token.text == 'FORMAT':
-        return len(self.token)
+        in_format = True
 
-    return 0
+    if not in_format:
+      return 0
+
+    score = 0
+
+    # check token matches an approved pattern
+    for p in self.approveds:
+      if p.match(self.token):
+        score = len(self.token)
+
+    return score
