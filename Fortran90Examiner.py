ae9de5967122d8889999119b6aad1c117765d147
John Fitzpatrick <jfitz@computer.org>
2019-01-06 11:13:57 -0800

    Examiner and token builders for Fortran-90.

---
+++ Fortran90Examiner.py
@@ -0,0 +1,125 @@
+import string
+from Examiner import Examiner
+from TokenBuilders import *
+from FortranXTokenBuilders import *
+from Fortran90TokenBuilders import *
+from Tokenizer import Tokenizer
+
+class Fortran90Examiner(Examiner):
+  def __init__(self, code, tab_size):
+    super().__init__()
+
+    num_known_tokens = 0
+    num_invalid_operators = 0
+    num_known_operators = 0
+
+    whitespace_tb = WhitespaceTokenBuilder()
+    newline_tb = NewlineTokenBuilder()
+
+    integer_tb = IntegerTokenBuilder(False)
+    integer_exponent_tb = IntegerExponentTokenBuilder()
+    real_tb = RealTokenBuilder(False, False)
+    real_exponent_tb = RealExponentTokenBuilder(False, False)
+    identifier_tb = IdentifierTokenBuilder()
+
+    bang_comment_tb = LeadCommentTokenBuilder('!')
+    string_tb = StringTokenBuilder(["'", '"'])
+
+    known_operators = [
+      '=', '+', '-', '*', '/', '**',
+      '==', '>', '>=', '<', '<=', '/=',
+      '.EQ.', '.NE.', '.LT.', '.LE.', '.GT.', '.GE.',
+      '.AND.', '.OR.', '.NOT.', '.EQV.', '.NEQV.',
+      '(', ')', ',',
+      ':', '::', '[', ']'
+      ]
+    
+    known_operator_tb = ListTokenBuilder(known_operators, 'operator', False)
+
+    continuation_tb = ListTokenBuilder(['&'], 'line continuation', False)
+
+    stmt_separator_tb = ListTokenBuilder([';'], 'statement separator', False)
+
+    keywords = [
+      'IF', 'THEN', 'ELSE', 'ENDIF', 'END IF', 'GO', 'TO', 'GOTO', 'GO TO',
+      'ASSIGN',
+      'READ', 'WRITE', 'BACKSPACE', 'REWIND', 'ENDFILE', 'FORMAT', 'PRINT',
+      'DO', 'CONTINUE',
+      'PROGRAM', 'SUBROUTINE', 'FUNCTION', 'BLOCK DATA',
+      'INTEGER', 'REAL', 'COMPLEX', 'DOUBLE PRECISION', 'LOGICAL', 'CHARACTER',
+      'IMPLICIT', 'SAVE',
+      'COMMON', 'DIMENSION', 'EQUIVALENCE', 'DATA', 'EXTERNAL',
+      'CALL', 'RETURN', 'PAUSE', 'STOP', 'END',
+      'INQUIRE', 'INTRINSIC', 'PARAMETER',
+      'allocatable', 'allocate', 'case', 'contains', 'cycle', 'deallocate',
+      'elsewhere', 'exit', 'include', 'interface', 'intent', 'module',
+      'namelist', 'nullify', 'only', 'operator', 'optional', 'pointer',
+      'private', 'procedure', 'public', 'recursive', 'result', 'select',
+      'sequence', 'target', 'use', 'while', 'where',
+      'enddo', 'none'
+      ]
+
+    keyword_tb = ListTokenBuilder(keywords, 'keyword', False)
+
+    tokenbuilders = [
+      whitespace_tb,
+      newline_tb,
+      integer_tb,
+      integer_exponent_tb,
+      real_tb,
+      real_exponent_tb,
+      keyword_tb,
+      identifier_tb,
+      string_tb,
+      bang_comment_tb,
+      continuation_tb,
+      stmt_separator_tb,
+      known_operator_tb,
+      self.unknown_operator_tb
+    ]
+
+    invalid_token_builder = InvalidTokenBuilder()
+    tokenizer = Tokenizer(tokenbuilders, invalid_token_builder)
+
+    self.tokens = tokenizer.tokenize(code)
+
+    num_known_tokens = self.count_valid_tokens(self.tokens)
+    num_invalid_operators = self.count_invalid_operators(self.tokens)
+    num_known_operators = self.count_known_operators(self.tokens)
+
+    # unknown tokens reduce confidence
+    token_confidence = 1.0
+    if len(self.tokens) > 0:
+      token_confidence = num_known_tokens / len(self.tokens)
+
+    #  unknown operators reduce confidence
+    operator_confidence = 1.0
+    num_operators = num_known_operators + num_invalid_operators
+    if num_operators > 0:
+      operator_confidence = num_known_operators / num_operators
+
+    # two operands in a row is not FORTRAN
+    tokens = self.drop_whitespace(self.tokens)
+    tokens = self.drop_comments(tokens)
+
+    operands = ['number', 'string', 'identifier']
+
+    two_operand_count = 0
+    prev_token = Token('\n', 'newline')
+    for token in tokens:
+      if token.group in operands and prev_token.group in operands:
+        two_operand_count += 1
+
+      prev_token = token
+
+    operand_confidence = 1.0
+    if len(tokens) > 0:
+      operand_confidence = 1.0 - (two_operand_count / len(tokens))
+
+    # compute confidence
+    self.confidence = token_confidence * operator_confidence * operand_confidence
+    self.confidences = {
+      'token': token_confidence,
+      'operator': operator_confidence,
+      'operand': operand_confidence
+      }
