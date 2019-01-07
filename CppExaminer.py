f2923135f6c0a0cfcb1591b2b61eea9c27ab9afe
John Fitzpatrick <jfitz@computer.org>
2019-01-06 23:39:58 -0800

    FOr C++ provide the complete set of keywords, including those from C++20.

--- CppExaminer.py
+++ CppExaminer.py
@@ -40,22 +40,26 @@ class CppExaminer(Examiner):
     known_operator_tb = ListTokenBuilder(known_operators, 'operator', True)
 
     keywords = [
-      'int', 'char', 'float', 'double',
-      'signed', 'unsigned', 'short', 'long',
-      'typedef', 'enum',
-      'auto', 'extern', 'register', 'static',
-      'for', 'while', 'do',
-      'const', 'volatile', 'sizeof',
-      'if', 'else', 'switch', 'case', 'default',
-      'struct', 'union', 'void', 'return',
-      'goto', 'continue', 'break',
-      'new', 'delete', 'explicit', 'mutable',
-      'private', 'protected', 'public',
-      'true', 'false',
-      'cin', 'cout',
-      'bool', 'class', 'friend', 'operator',
-      'try', 'catch', 'throw'
-      ]
+      'alignas', 'alignof', 'and', 'and_eq', 'asm', 'atomic_cancel',
+      'atomic_commit', 'atomic_noexcept', 'auto', 'bitand', 'bitor',
+      'bool', 'break', 'case', 'catch', 'char', 'char8_t', 'char16_t',
+      'char32_t', 'class', 'compl', 'concept', 'const', 'consteval',
+      'constexpr', 'const_cast', 'continue', 'co_await', 'co_return',
+      'co_yield', 'decltype', 'default', 'delete', 'do', 'double',
+      'dynamic_cast', 'else', 'enum', 'explicit', 'export', 'extern',
+      'false', 'float', 'for', 'friend', 'goto', 'if', 'import',
+      'inline', 'int', 'long', 'module', 'mutable', 'namespace', 'new',
+      'noexcept', 'not', 'not_eq', 'nullptr', 'operator', 'or', 'or_eq',
+      'private', 'protected', 'public', 'reflexpr', 'register',
+      'reinterpret_cast', 'requires', 'return', 'short', 'signed',
+      'sizeof', 'static', 'static_assert', 'static_cast', 'struct',
+      'switch', 'synchronized', 'template', 'this', 'thread_local',
+      'throw', 'true', 'try', 'typedef', 'typeid', 'typename', 'union',
+      'unsigned', 'using', 'virtual', 'void', 'volatile', 'wchar_t',
+      'while', 'xor', 'xor_eq', 'cout', 'cin',
+      'override', 'axiom', 'final', 'audit', 'transaction_safe',
+      'transaction_safe_dynamic'
+    ]
 
     keyword_tb = ListTokenBuilder(keywords, 'keyword', True)
 
