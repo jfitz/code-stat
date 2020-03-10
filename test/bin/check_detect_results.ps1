0d2e8a76f42bedf79f89803d735004f7281ae211
John Fitzpatrick <jfitz@computer.org>
2020-03-10 10:14:34 -0700

    check_detect_results script ignores non-code and multiple languages.

--- test/bin/check_detect_results.ps1
+++ test/bin/check_detect_results.ps1
@@ -7,25 +7,29 @@ $names = @{
     'PL/1-Free'='PL1'
 }
 
+$ignore_names = 'noncode', 'multiple'
+
 Get-ChildItem .\test\ref\detect |
 ForEach-Object {
     $language = $_.BaseName
-    Get-ChildItem $_ -Filter *.txt |
-    ForEach-Object {
-        $test = $_.BaseName
-        $a = Get-Content $_ | Select-String ":"
-        $c = $a -replace ":", " " -replace '"', '' -replace ',', '' | Sort-Object { [double]$_.split()[-1] } -Descending | Select-Object -first 1
-        if (-Not ([string]::IsNullOrEmpty($c))) {
-            $d = $c.Trim()
-            $detected, $confidence = $d.Split(' ', 2)
+    if (-Not $ignore_names.Contains($language)) {
+        Get-ChildItem $_ -Filter *.txt |
+        ForEach-Object {
+            $test = $_.BaseName
+            $a = Get-Content $_ | Select-String ":"
+            $c = $a -replace ":", " " -replace '"', '' -replace ',', '' | Sort-Object { [double]$_.split()[-1] } -Descending | Select-Object -first 1
+            if (-Not ([string]::IsNullOrEmpty($c))) {
+                $d = $c.Trim()
+                $detected, $confidence = $d.Split(' ', 2)
 
-            # map detected name to standard name
-            If ($names.ContainsKey($detected)) {
-                Set-Variable -Name detected -Value $names[$detected]
-            }
+                # map detected name to standard name
+                If ($names.ContainsKey($detected)) {
+                    Set-Variable -Name detected -Value $names[$detected]
+                }
 
-            If ($detected -ne $language) {
-                Write-Output "$language $test $detected $confidence"
+                If ($detected -ne $language) {
+                    Write-Output "$language $test $detected $confidence"
+                }
             }
         }
     }
